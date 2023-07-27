dd9db28a727ea201f3121a0600b5588786a2d955
beto0077 <gilberto.gn.1997@gmail.com>
2023-07-27 03:35:08 -0600

    index.routes updated

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -4,9 +4,10 @@ import {pool} from '../db.js';
 const router = Router();
 
 router.get("/ping", async (req, res) => {
-    const [rows] = await pool.query("SELECT 1 + 1 as result");
+    /*const [rows] = await pool.query("SELECT 1 + 1 as result");
     console.log(rows[0]);
-    res.json(rows[0]);
+    res.json(rows[0]);*/
+    res.send({message: 'Hello Dark World!'});
 });
 
 export default router;
